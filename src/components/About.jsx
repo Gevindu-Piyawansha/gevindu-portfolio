81b824fdce733f11821ce5641bec4192ebde18b1
gevindu <gevindu.piyawansha@gmail.com>
2025-06-20 12:20:43 +0200

    Fix: export default About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I am an aspiring fullstack developer with a strong background in Java and web technologies. Transitioning from a career in Civil Engineering, I have completed the Comprehensive Master Java Developer program at IJSE and recently earned my MSc in Construction and Real Estate Management from Riga Technical University. Currently seeking junior roles in Frontend, Backend, Fullstack, or Internships in Norway. Open to relocation and continuously learning.
+      </p>
+    </section>
+  );
+}
+
+export default About;
