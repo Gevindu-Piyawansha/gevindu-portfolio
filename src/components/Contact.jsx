3c63e81d60160cd4737936998c6887f9d921d95a
gevindu <gevindu.piyawansha@gmail.com>
2025-06-20 12:32:02 +0200

    Fix: force export Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Email: <a href="mailto:gevindu.piyawansha@gmail.com">gevindu.piyawansha@gmail.com</a></p>
+      <p>Phone: +47 99875112</p>
+      <p>GitHub: <a href="https://github.com/Gevindu-Piyawansha" target="_blank">Gevindu-Piyawansha</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/gevindu-piyawansha/" target="_blank">Gevindu-Piyawansha</a></p>
+    </section>
+  );
+}
+
+export default Contact;
