1be18d7cd99436de9fc66375282163315d2eea4f
gevindu <gevindu.piyawansha@gmail.com>
2025-06-20 12:27:19 +0200

    Fix: export default Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,22 @@
+function Skills() {
+    return (
+        <section id="skills">
+            <h2>Skills</h2>
+            <ul>
+                <li>Java</li>
+                <li>Spring Boot</li>
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>MySQL</li>
+                <li>Git</li>
+                <li>REST APIs</li>
+                <li>Hibernate</li>
+                <li>Maven</li>
+            </ul>
+        </section>
+    );
+}
+
+export default Skills;
