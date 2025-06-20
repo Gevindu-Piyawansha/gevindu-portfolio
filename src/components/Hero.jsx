f560429cf586b50f52a358e583b3ecfffa3f7ab4
gevindu <gevindu.piyawansha@gmail.com>
2025-06-20 12:13:43 +0200

    Initial commit - Vite React Portfolio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+        <section id="hero">
+            <h1>Gevindu Piyawansha</h1>
+            <h2>Aspiring Fullstack Developer | Java | Spring Boot | React | Based in Norway</h2>
+        </section>
+    );
+}
+
+export default Hero;
